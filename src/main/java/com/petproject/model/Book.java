36f515e3863101f9c832bf8f11df83eb972cf24a
pg312 <vpadmagade@gmail.com>
2024-03-09 15:54:24 +0530

    Added not null constraints and more properties

--- src/main/java/com/petproject/model/Book.java
+++ src/main/java/com/petproject/model/Book.java
@@ -1,13 +1,15 @@
 package com.petproject.model;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import lombok.Generated;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Data
@@ -17,9 +19,25 @@ public class Book {
     @GeneratedValue
     @Id
     private Long id;
+
+    @NotNull
     private String name;
+
+    @NotNull
     private String author;
 
+
+    @NotNull
     private int price;
+
+    @Column(length = 150)
+    @NotNull
+    private String description;
+
+    @NotNull
+    private String publisher;
+
+    @NotNull
+    private List<String> availableLanguages;
 
 }
